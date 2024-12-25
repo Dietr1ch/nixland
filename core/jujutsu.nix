94d91ad52b77e40f76c746606da0c056d93a2242
Dietrich Daroch <Dietrich@Daroch.me>
2024-12-25 04:03:32 -0300

    Misc: Add links

--- core/jujutsu.nix
+++ core/jujutsu.nix
@@ -5,9 +5,12 @@
     jujutsu = {
       enable = true;
 
+      # https://github.com/jj-vcs/jj/tree/main/docs/config.md
+      # ~/.config/jj/config.toml
       settings = {
         ui = {
           allow-init-native = true;
+
           default-command = "log";
         };
       };
