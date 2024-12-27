83564e798d254644d32704b9f0f6199937a8825c
Dietrich Daroch <Dietrich@Daroch.me>
2024-12-27 15:07:32 -0300

    Core: Add hunspell

--- desktop/editor/default.nix
+++ desktop/editor/default.nix
@@ -1,8 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./emacs.nix
     ./helix.nix
   ];
+
+  home = {
+    sessionVariables = {
+      DICTIONARY = "en_GB";
+    };
+
+    packages = with pkgs; [
+      hunspell
+    ];
+  };
 }
