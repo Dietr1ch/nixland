be3f7a706b22c1459878485ec1141493a30dc200
Dietrich Daroch <Dietrich@Daroch.me>
2025-05-12 20:07:21 -0400

    Gaming,SDL: Update SDL environment.
    Let's try to drop X11 support

--- gaming/sdl.nix
+++ gaming/sdl.nix
@@ -3,8 +3,12 @@
 {
   home = {
     sessionVariables = {
-      "SDL_VIDEODRIVER" = "wayland,x11";
+      # SDL2 (https://github.com/libsdl-org/SDL/blob/SDL2/include/SDL_hints.h)
+      "SDL_VIDEODRIVER" = "wayland";
       "SDL_AUDIODRIVER" = "pipewire";
+      # SDL3 (https://github.com/libsdl-org/SDL/blob/main/include/SDL3/SDL_hints.h)
+      "SDL_VIDEO_DRIVER" = "wayland";
+      "SDL_AUDIO_DRIVER" = "pipewire";
     };
   };
 }
