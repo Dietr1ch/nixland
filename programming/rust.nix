7197d7cb84b4841b0ac2e0f05c2d484155709272
Dietrich Daroch <Dietrich@Daroch.me>
2025-05-17 17:43:57 -0400

    Rust: Drop cargo aliases. Let's just be explicit

--- programming/rust.nix
+++ programming/rust.nix
@@ -11,17 +11,6 @@
     file = {
       ".cargo/config.toml" = {
         text = ''
-          [alias]
-          b  = "build --release --keep-going"
-          bb = "build --release --keep-going --all-targets"
-          r  = "run"
-          rr = "run --release"
-
-          c = "check"
-          cc = "check --release"
-          t = "test"
-          tt = "test --release"
-
           # [doc]
           browser = "$BROWSER --new-window"
 
