a64dfd2fe2331c2ffd3b1ffd697f0c137e79ff8d
Dietrich Daroch <Dietrich@Daroch.me>
2025-05-31 18:08:13 -0400

    Gaming,Wine: Use regular wayland wine instead of waylandFull

--- gaming/wine.nix
+++ gaming/wine.nix
@@ -5,14 +5,15 @@
     packages = with pkgs; [
       lutris
 
-      wine
+      # wine
       winetricks
       # 32-bit support
       # support both 32- and 64-bit applications
       # wineWowPackages.stable
       # wine-staging (version with experimental features)
       # wineWowPackages.staging
-      wineWowPackages.waylandFull # (Unstable)
+      wineWowPackages.wayland # (Unstable)
+      # wineWowPackages.waylandFull # (Unstable)
     ];
   };
 }
