dd308b4b0a19fa40c24ea754e3710d52fa184f82
Dietrich Daroch <Dietrich@Daroch.me>
2025-06-13 08:28:00 -0700

    Haskell: Add ghci config

--- programming/haskell.nix
+++ programming/haskell.nix
@@ -3,6 +3,33 @@
 {
   home = {
     packages = with pkgs; [ ghc ];
+
+    file = {
+      ".config/ghc/ghci.conf" = {
+        text = ''
+          -- Prompt
+          :set prompt "\x03BB> "
+          :set prompt-cont " > "
+
+          -- Extensions
+          :seti -XGADTSyntax
+          :seti -XGeneralizedNewtypeDeriving
+          :seti -XInstanceSigs
+          :seti -XLambdaCase
+          :seti -XPartialTypeSignatures
+          :seti -XScopedTypeVariables
+          :seti -XTypeApplications
+          :seti -XOverloadedStrings
+
+          -- Constants
+          let kb = 1024;
+          let mb = 1024 * kb;
+          let gb = 1024 * mb;
+          let tb = 1024 * gb;
+          let pb = 1024 * tb;
+        '';
+      }; # ..home.file.".config/ghc/ghci.conf"
+    }; # ..home.file
   }; # ..home
 
 }
