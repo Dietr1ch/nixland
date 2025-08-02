769387091fecec077d8e738748ce9c41589e18d1
Dietrich Daroch <Dietrich@Daroch.me>
2025-08-02 16:30:41 -0400

    Programming,Rust: Add tools that I reach out for in random projects

--- programming/rust.nix
+++ programming/rust.nix
@@ -12,16 +12,26 @@
       cargo
       cargo-audit
       cargo-benchcmp
+      cargo-bloat
       cargo-criterion
       cargo-deny
       cargo-edit
       cargo-expand
       cargo-flamegraph
+      cargo-fuzz
+      cargo-modules
+      cargo-nextest
       cargo-outdated
+      cargo-pgo
       cargo-public-api
+      cargo-semver-checks
+      cargo-show-asm
       cargo-spellcheck
       cargo-toml-lint
+      cargo-udeps
+      cargo-valgrind
       cargo-watch
+
       critcmp
     ]; # ..home.packages
 
