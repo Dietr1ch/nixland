ad0c9951fe6691881159dc65d3c118f493a67542
Dietrich Daroch <Dietrich@Daroch.me>
2025-08-31 19:40:13 -0400

    SSH: Update config

--- core/ssh.nix
+++ core/ssh.nix
@@ -10,18 +10,25 @@
       # --------
       # ~/.ssh/config
 
-      compression = false;
-      controlMaster = "auto";
-      controlPersist = "3m";
-      controlPath = "$\{XDG_RUNTIME_DIR\}/ssh-socket-%l-%r@%h:%p";
-      serverAliveInterval = 5; # 5s
-      serverAliveCountMax = 3; # Up to 3 in-flight heartbeats
+      matchBlocks = {
+        "*" = {
+          addKeysToAgent = "no";
 
-      # ~/.config/ssh/known_hosts
-      userKnownHostsFile = "$\{XDG_CONFIG_HOME\}/ssh/known_hosts";
+          compression = false;
+          controlMaster = "auto";
+          controlPersist = "3m";
+          controlPath = "$\{XDG_RUNTIME_DIR\}/ssh-socket-%l-%r@%h:%p";
+          serverAliveInterval = 5; # 5s
+          serverAliveCountMax = 3; # Up to 3 in-flight heartbeats
 
-      hashKnownHosts = true;
-      forwardAgent = false;
+          # ~/.config/ssh/known_hosts
+          userKnownHostsFile = "$\{XDG_CONFIG_HOME\}/ssh/known_hosts";
+
+          hashKnownHosts = true;
+          forwardAgent = false;
+
+        };
+      };
 
       extraConfig = ''
         Include ''${XDG_CONFIG_HOME}/ssh/config.d/*
