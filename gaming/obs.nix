a5f871a2285331a173b88880a9e256aa6df9c648
Dietrich Daroch <Dietrich@Daroch.me>
2025-09-03 15:10:01 -0400

    Add a bunch of OBS plugins

--- gaming/obs.nix
+++ gaming/obs.nix
@@ -4,8 +4,15 @@
   programs = {
     obs-studio = {
       enable = true;
-      plugins = with pkgs; [
-        # obs-studio-plugins.obs-ndi
+      plugins = with pkgs.obs-studio-plugins; [
+        # BROKEN: distroav # NDI-6 is broken
+        input-overlay
+        obs-advanced-masks
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+        obs-source-switcher
+        obs-vkcapture
+        wlrobs
       ];
     };
   };
