bc081dbdbe48b0d5ccc3eeadb528aecd9b8029c2
Dietrich Daroch <Dietrich@Daroch.me>
2025-09-03 19:40:45 -0400

    Add OBS plugins

--- gaming/obs.nix
+++ gaming/obs.nix
@@ -2,15 +2,19 @@
 
 {
   programs = {
+    # https://home-manager-options.extranix.com/?release=master&query=programs.obs-studio
     obs-studio = {
       enable = true;
+
       plugins = with pkgs.obs-studio-plugins; [
         # BROKEN: distroav # NDI-6 is broken
         input-overlay
         obs-advanced-masks
         obs-backgroundremoval
+        obs-gstreamer
         obs-pipewire-audio-capture
         obs-source-switcher
+        obs-vaapi # optional AMD hardware acceleration
         obs-vkcapture
         wlrobs
       ];
