0cdc319ffef2819e36cd47b43ce5d0eb5b95c9f6
Dietrich Daroch <Dietrich@Daroch.me>
2025-09-03 20:00:55 -0400

    Mark emulationstation as broken

--- gaming/emulators.nix
+++ gaming/emulators.nix
@@ -4,7 +4,7 @@
 {
   home = {
     packages = with pkgs; [
-      emulationstation-de
+      # BROKEN: emulationstation-de  # error: Package ‘freeimage-3.18.0-unstable-2024-04-18’ in /nix/store/8ly89qdcjh6pb5xamvq4vrzqnifwshn3-source/pkgs/by-name/fr/freeimage/package.nix:114 is marked as insecure, refusing to evaluate.
 
       retroarch
       retroarch-joypad-autoconfig
