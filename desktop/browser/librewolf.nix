40d33af79373a844bbb9bd741f97e99df90dbd88
Dietrich Daroch <Dietrich@Daroch.me>
2025-09-19 15:11:38 -0300

    Browser: Use programs.librewolf over raw package include

--- desktop/browser/librewolf.nix
+++ desktop/browser/librewolf.nix
@@ -1,12 +1,20 @@
 { pkgs, ... }:
 
 {
-  # programs = {
-  #   librewolf = {
-  #     enable = true;
-  #     # package = pkgs.librewolf-wayland;
-  #   };  # ..programs.librewolf
-  # };  # ..programs
+  programs = {
+    librewolf = {
+      enable = true;
+
+      package = pkgs.librewolf-wayland;
+
+      # https://librewolf.net/docs/settings/
+      # settings = {
+      #   # "webgl.disabled" = false;
+      #   # "privacy.resistFingerprinting.letterboxing" = true;
+      #   # "privacy.resistFingerprinting" = false;
+      # };
+    }; # ..programs.librewolf
+  }; # ..programs
 
   home = {
     packages = with pkgs; [
