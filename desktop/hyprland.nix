4fcb97b1c67b61a26ae1fb9d4d770d7eec59c2ce
Dietrich Daroch <Dietrich@Daroch.me>
2025-09-22 13:30:10 -0300

    Desktop,Hyprland: Remove old gesture option and list the defaults.

--- desktop/hyprland.nix
+++ desktop/hyprland.nix
@@ -159,7 +159,19 @@ in
           };
           # https://wiki.hyprland.org/Configuring/Variables/#gestures
           gestures = {
-            workspace_swipe = true;
+            # Defaults
+            # workspace_swipe_distance = 300; # 300px. The distance of the touchpad gesture
+            # workspace_swipe_touch = false; # Enable workspace swiping from the edge of a touchscreen
+            # workspace_swipe_invert = true; # Invert the direction (touchpad only)
+            # workspace_swipe_touch_invert = false; # Invert the direction (touchscreen only)
+            # workspace_swipe_min_speed_to_force = 30; # 30px/tick. Minimum speed in px per timepoint to force the change ignoring cancel_ratio. Setting to 0 will disable this mechanic.
+            # workspace_swipe_cancel_ratio = 0.5; # [0.0 - 1.0]. How much the swipe has to proceed in order to commence it. (0.7 -> if > 0.7 * distance, switch, if less, revert)
+            # workspace_swipe_create_new = true; # Whether a swipe right on the last workspace should create a new one
+            # workspace_swipe_direction_lock = true; # If enabled, switching direction will be locked when you swipe past the direction_lock_threshold (touchpad only)
+            # workspace_swipe_direction_lock_threshold = 10; # 10px. The distance to swipe before direction lock activates (touchpad only)
+            # workspace_swipe_forever = false; # If enabled, swiping will not clamp at the neighboring workspaces but continue to the further ones
+            # workspace_swipe_use_r = false; # If enabled, swiping will use the r prefix instead of the m prefix for finding workspaces
+            # close_max_timeout = 1000; # 1000ms. The timeout for a window to close when using a 1:1 gesture
           };
 
           # Programs
