2327e672c0a209b44a3cfa5ded380c0152c093a9
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-11 15:13:44 -0300

    Shell,History: Tweak the atuin config

--- shell/history.nix
+++ shell/history.nix
@@ -13,6 +13,12 @@
       enableBashIntegration = true;
       enableNushellIntegration = true;
 
+      # Flags passed to `atuin init $SHELL`.
+      flags = [
+        # https://docs.atuin.sh/configuration/key-binding/#disable-up-arrow
+        "--disable-up-arrow"
+      ];
+
       daemon = {
         enable = true;
         logLevel = "info";
@@ -28,9 +34,43 @@
         # sync_frequency = "5m";
         # sync_address = "https://api.atuin.sh";
 
+        # Search
+        # ------
+        # https://docs.atuin.sh/configuration/config/#workspaces
+        workspaces = true;
+        # https://docs.atuin.sh/configuration/config/#filter_mode_shell_up_key_binding
+        filter_mode_shell_up_key_binding = "directory";
         # https://docs.atuin.sh/configuration/config/#search_mode
         # https://github.com/skim-rs/skim#search-syntax
         search_mode = "skim";
+
+        # Matching
+        # https://docs.atuin.sh/configuration/config/#common_subcommands
+        common_subcommands = [
+          "apt"
+          "cargo"
+          "composer"
+          "dnf"
+          "docker"
+          "git"
+          "go"
+          "home-manager"
+          "ip"
+          "kubectl"
+          "nix flake"
+          "nix"
+          "nix-store"
+          "nixos-rebuild"
+          "nmcli"
+          "npm"
+          "pecl"
+          "pnpm"
+          "podman"
+          "port"
+          "systemctl"
+          "tmux"
+          "yarn"
+        ];
       };
     }; # ..programs.atuin
   }; # ..programs
