38ae50649afe79c07f769e408063a0c6c3960309
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-01 19:10:33 -0300

    Desktop,Sync: Change the default GUI address to a socket

--- desktop/sync.nix
+++ desktop/sync.nix
@@ -5,6 +5,9 @@
     # https://home-manager-options.extranix.com/?release=master&query=services.syncthing
     syncthing = {
       enable = true;
+
+      # https://www.freedesktop.org/software/systemd/man/latest/systemd.unit.html#Specifiers
+      guiAddress = "%t/%N.socket";
       tray = {
         enable = true;
       };
