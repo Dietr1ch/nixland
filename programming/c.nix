b273dab9e40399cd82d0c9d0a0f3e53e31c9217b
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-03 02:32:20 -0300

    Misc: Add links

--- programming/c.nix
+++ programming/c.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 
+# https://nixos.wiki/wiki/C
 {
   home = {
+    # Packages in sync with ~/Projects/systemn/desktop/programming/c.nix
     packages = with pkgs; [
       gcc
       gdb
--- programming/cpp.nix
+++ programming/cpp.nix
@@ -3,6 +3,7 @@
 # https://nixos.wiki/wiki/C
 {
   home = {
+    # Packages in sync with ~/Projects/systemn/desktop/programming/cpp.nix
     packages = with pkgs; [
       gcc
       gdb
