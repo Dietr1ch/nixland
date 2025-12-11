a50bb28c7ff86bfb53eb007c692c17b80309f170
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-10 23:45:52 -0300

    Gaming,MangoHud: Drop preconfigured config
    Nix attribute sets are sorted, which kind of ruins the output

--- gaming/mangohud.nix
+++ gaming/mangohud.nix
@@ -8,12 +8,6 @@
 
       enableSessionWide = false;
 
-      # ~/.config/MangoHud/MangoHud.conf
-      # https://github.com/flightlessmango/MangoHud/blob/master/data/MangoHud.conf
-      settings = {
-        output_folder = "/tmp/mangohud/";
-      };
-
       settingsPerApplication = {
         # ~/.config/MangoHud/mpv.conf
         "mpv" = {
