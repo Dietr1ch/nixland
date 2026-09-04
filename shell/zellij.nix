905fe8683952a15fb14164d60dea4f2aa18d903b
Dietrich Daroch <Dietrich@Daroch.me>
2026-09-04 15:59:10 -0400

    zellij: Add forgot

--- shell/zellij.nix
+++ shell/zellij.nix
@@ -70,7 +70,7 @@ in
 
           // Extra plugins (https://github.com/zellij-org/awesome-zellij)
           sessionizer location="file:///home/dietr1ch/.config/zellij/plugins/sessionizer.wasm"
-          // forgot location="file:///home/dietr1ch/.config/zellij/plugins/forgot.wasm"
+          forgot location="file:///home/dietr1ch/.config/zellij/plugins/forgot.wasm"
         }
 
         // Plugins to load in the background when a new session starts
@@ -111,6 +111,11 @@ in
                     floating true
                 }
             }
+            bind "Alt Shift /" {
+              LaunchOrFocusPlugin "forgot" {
+                floating true
+              };
+            }
 
             // Sessions
             bind "Ctrl q" { Detach; }
@@ -131,10 +136,6 @@ in
                 session_layout "${default_layout}"
               };
             }
-            // bind "Alt Shift /" {
-            //   LaunchOrFocusPlugin "forgot" {
-            //   };
-            // }
             // Sessions - Descend / Ascend
             bind "Alt Shift s" { FocusGuestSession; }
             bind "Alt Ctrl Shift s" { FocusHostSession; }
@@ -366,11 +367,11 @@ in
         sha256 = "sha256-xBhBwCPnToH5mg/Y2V4FBO0gLfLNuSYE31HJ5OoLoFs=";
       };
 
-      # ".config/zellij/plugins/forgot.wasm".source = pkgs.fetchurl {
-      #   # https://github.com/karimould/zellij-forgot/releases
-      #   url = "https://github.com/karimould/zellij-forgot/releases/download/0.4.2/zellij_forgot.wasm"; # Latest as of 2026-09-04
-      #   sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-      # };
+      ".config/zellij/plugins/forgot.wasm".source = pkgs.fetchurl {
+        # https://github.com/karimould/zellij-forgot/releases
+        url = "https://github.com/karimould/zellij-forgot/releases/download/0.4.2/zellij_forgot.wasm"; # Latest as of 2026-09-04
+        sha256 = "sha256-MRlBRVGdvcEoaFtFb5cDdDePoZ/J2nQvvkoyG6zkSds=";
+      };
     }; # ..home.file
   }; # ..home
 }
